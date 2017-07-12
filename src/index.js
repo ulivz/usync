7bf19999645f4caad08ea5a9b866ab8a4d38f214
toxichl <472590061@qq.com>
2017-07-12 20:35:34 +0800

    Update readme and delete some processing files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/// <reference path="./index.d.ts" />
-var Usync_1 = require("./core/Usync");
-module.exports = Usync_1.Usync;
-//# sourceMappingURL=index.js.map
