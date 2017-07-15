9e0a3135535be5babd56bd4dc030c64bcb0a763f
toxichl <472590061@qq.com>
2017-07-15 21:56:34 +0800

    update logger plugin

--- plugins/logger.js
+++ plugins/logger.js
@@ -5,13 +5,24 @@ function logWithTime(content) {
     console.log(chalk.gray(time) + ' ' + content)
 }
 
+function indentByDepth(depth) {
+    var indent = ''
+    while (depth >= 0){
+        indent += '   '
+        depth--
+    }
+    return indent
+}
+
 module.exports = function (Usync) {
     Usync.extend({
         taskStart(root) {
-            logWithTime(` Starting ${chalk.cyan(root.$current.name)} ...`)
+            logWithTime(`${indentByDepth(root.$current.$parent ? root.$current.$parent.depth + 1 : 0)}Starting ${chalk.cyan(root.$current.name)} ...`)
+
         },
         taskEnd(root) {
-            logWithTime(` ${chalk.redBright('Finished')} ${chalk.cyan(root.$current.name)} after ${chalk.magenta((root.$current.endTime - root.$current.startTime) + 'ms')}`)
+
+            logWithTime(`${indentByDepth(root.$current.$parent ? root.$current.$parent.depth + 1 : 0)}${chalk.redBright('Finished')} ${chalk.cyan(root.$current.name)} after ${chalk.magenta((root.$current.endTime - root.$current.startTime) + 'ms')}`)
         }
     })
 }
